b15f3aad6b7151aa8261ee0c49d2e0d3ee09b2f0
toast <toast@toast-server.net>
2023-12-06 22:13:05 +1100

    Add WebSocket latency

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -2,9 +2,7 @@ use crate::Error;
 
 /// Check if the bot is alive
 #[poise::command(slash_command)]
-pub async fn ping(
-  ctx: poise::Context<'_, (), Error>,
-) -> Result<(), Error> {
-  ctx.reply("Powong!").await?;
+pub async fn ping(ctx: poise::Context<'_, (), Error>) -> Result<(), Error> {
+  ctx.reply(format!("Powong! `{:?}`ms", ctx.ping().await.as_millis())).await?;
   Ok(())
 }
